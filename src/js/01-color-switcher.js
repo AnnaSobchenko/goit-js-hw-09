3fc0075116d38e372bef130971c27d0b1c8cd772
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-10 18:49:07 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const backgroundColorEl = document.querySelector('body');
 
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setBackgroundColor() {
+  colorBackChange = setInterval(() => {
+    backgroundColorEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  console.log(startBtn);
+  console.log(stopBtn);
+}
+
+startBtn.addEventListener('click', setBackgroundColor);
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(colorBackChange);
+  //   startBtn.removeEventListener('click', setBackgroundColor);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  console.log(`Change backgroundcolor has stopped!`);
+  console.log(startBtn);
+  console.log(stopBtn);
+});

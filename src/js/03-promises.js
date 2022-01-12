fa5cdec50f6afef094e761cb8aa43be98d5e6f64
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-12 22:32:10 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,29 +1,34 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmit);
 
-console.log(formEl.amount);
+function onSubmit(e) {
+  e.preventDefault();
+  const delay = Number(formEl.elements['delay'].value);
+  const step = Number(formEl.elements['step'].value);
+  const amount = Number(formEl.elements['amount'].value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay + step * (i - 1))
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
 
 function createPromise(position, delay) {
   return new Promise((res, rej) => {
-    setTimeout(() =>{
+    setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-   res({})
-  } else {
-    // Reject
-  }
-  })
-  
-  
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
 }
-
-// return new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     const canProcess = Math.random() > 0.3;
-//     if (canProcess) {
-//       resolve({ id: transaction.id, time: delay });
-//     }
-//     reject(transaction.id);
-//   }, delay);
-// });

db65eed3763552c16e4612f528eee59bdeac11fb
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-12 21:07:17 +0200

    update task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -40,8 +40,16 @@
 
 // Описан в документации
 import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
+
+let dateFuture;
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+console.log(daysEl);
 
 const options = {
   enableTime: true,
@@ -49,6 +57,58 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const today = new Date();
+    dateFuture = selectedDates[0];
+    if (dateFuture > today) {
+      startBtn.disabled = false;
+    } else {
+      startBtn.disabled = true;
+      // Notify.warning('Please choose a date in the future');
+    }
   },
 };
+
+const startBtn = document.querySelector('button[data-start]');
+startBtn.disabled = true;
+console.log(startBtn);
+flatpickr('#datetime-picker', options);
+// const dateFuture =
+
+// console.log(dateFuture.onClose);
+startBtn.addEventListener('click', onClick);
+
+function onClick() {
+  console.log(dateFuture);
+  updateTimer();
+  startBtn.disabled = true;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimer() {
+  const interval = setInterval(() => {
+    const ms = dateFuture - Date.now();
+    const { days, hours, minutes, seconds } = convertMs(ms);
+    daysEl.textContent = days;
+    hoursEl.textContent = hours;
+    minutesEl.textContent = minutes;
+    secondsEl.textContent = seconds;
+  }, 1000);
+}
